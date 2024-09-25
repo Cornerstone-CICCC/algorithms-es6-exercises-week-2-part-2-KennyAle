7bf7e52b4569b3fa577beb8d67bd38ddff9d7f80
KennyAle <kennyviver@gmail.com>
2024-09-25 11:43:53 -0700

    Exercise 6

--- ex06.js
+++ ex06.js
@@ -1,3 +1,7 @@
 // Every Exercise
 // Instructions: Create an array of at least five numbers. Write code to check if every number in the array is positive.
+const numbers = [-1, 2, 4]
+
+const value = numbers.every((item) => item >= 0)
+console.log(value)
 
